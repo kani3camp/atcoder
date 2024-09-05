865da489ce7cc1ef7b43de85da91674159e8e187
kani3camp <kani3camp@gmail.com>
2024-09-05 23:41:36 +0900

    C++対応

---
+++ AtCoder Beginners Selection/ABC085C.cpp
@@ -0,0 +1,13 @@
+//
+// Created by momom on 2024-09-05.
+//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!" << endl;
+  return 0;
+}
+
---
+++ AtCoder Beginners Selection/PracticeA - Welcome to AtCoder.cpp
@@ -0,0 +1,18 @@
+//
+// Created by momom on 2024-09-05.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    string s;
+
+    cin >> a;
+    cin >> b >> c;
+    cin >> s;
+
+    cout << a + b + c << " " << s << endl;
+
+    return 0;
+}
