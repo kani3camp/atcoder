a387ff4f0b19af62ecfd5c121a06fead1666a11e
kani3camp <sorarideblog@gmail.com>
2024-09-08 16:06:37 +0900

    sandbox

---
+++ C++ Sandbox/sandbox1.cpp
@@ -0,0 +1,12 @@
+//
+// Created by kani3camp on 2024/09/08.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    int a = 0b10111;
+    return 0;
+}
