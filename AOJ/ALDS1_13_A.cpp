803efc86c03334819e0a2112ecd6f7a771f2762b
kani3camp <sorarideblog@gmail.com>
2024-09-14 20:59:38 +0900

    update

---
+++ AOJ/ALDS1_13_A.cpp
@@ -0,0 +1,25 @@
+//
+// Created by kani3camp on 2024/09/14.
+//
+
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int k;
+    cin >> k;
+
+    int r[k], c[k];
+    for (int i = 1; i <= k; i++)
+    {
+        cin >> r[i] >> c[i];
+    }
+
+
+    return 0;
+}
