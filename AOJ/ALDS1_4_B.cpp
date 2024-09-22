516ab5470dd2e5c1b026f1e1395c018cfada10db
kani3camp <kani3camp@gmail.com>
2024-09-22 19:35:45 +0900

    update

--- AOJ/ALDS1_4_B.cpp
+++ AOJ/ALDS1_4_B.cpp
@@ -11,6 +11,35 @@ using namespace std;
 
 int main()
 {
+    int n, q;
+    cin >> n;
+    int S[n];
+    for (int i = 0; i < n; i++) {
+        cin >> S[i];
+    }
+    cin >> q;
+    int T[q];
+    for (int i = 0; i < q; i++) {
+        cin >> T[i];
+    }
+
+    int C = 0;
+    for (int i = 0; i < q; i++) {
+        int target = T[i];
+
+        int L = 0;
+        int R = n - 1;
+        while (L <= R) {
+            int M = (L + R) / 2;
+            if (target < S[M]) R = M - 1;
+            if (target == S[M]) {
+                C++; break;
+            }
+            if (target > S[M]) L = M + 1;
+        }
+    }
+
+    cout << C << endl;
 
     return 0;
 }
