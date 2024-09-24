a731d0810ac8d0fc9a927bd0a7ed3fe0bfefe779
kani3camp <sorarideblog@gmail.com>
2024-09-24 11:20:26 +0900

    update

---
+++ ABC/023/abc023_d.cpp
@@ -0,0 +1,26 @@
+//
+// Created by kani3camp on 2024/09/23.
+//
+
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin>>N;
+
+    int H[N], S[N];
+    for(int i=0; i<N; i++)
+    {
+        cin>>H[i]>>S[i];
+    }
+
+    
+
+    return 0;
+}
