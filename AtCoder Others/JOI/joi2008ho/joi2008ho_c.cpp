d6dfbd43aa3d4e70bb1038e284d87696b71eb366
kani3camp <sorarideblog@gmail.com>
2024-09-28 20:06:01 +0900

    update

---
+++ AtCoder Others/JOI/joi2008ho/joi2008ho_c.cpp
@@ -0,0 +1,27 @@
+//
+// Created by kani3camp on 2024/09/28.
+//
+
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+int P[200000000];
+
+int main()
+{
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> P[i];
+    }
+
+
+
+
+    return 0;
+}
